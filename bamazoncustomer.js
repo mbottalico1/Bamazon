38696dc3551f8d7bd4c912af22d85b4f903ce8cd
Michael Bottalico <michaelbottalico@Michaels-MBP.home>
2017-06-20 11:31:04 -0400

    updated

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -10,3 +10,25 @@ var connection = mysql.createConnection({
 	password: 'Mikebobo1@',
 	database: 'Bamazon'
 });
+
+connection.connect(function(err) {
+	if (err) throw err;
+	console.log('connected as id ' + connection.threadId);
+});
+
+
+/*
+connection.query('SELECT * FROM users WHERE id = ?', [userId], function (error, results, fields) {
+  if (error) throw error;
+  
+});
+*/
+function tableLog(res) {
+	var table = new Table({
+		head: ['Item ID', 'Product Name', 'Department', 'Price', 'Quantity'],
+	});
+	for (var i = 0; i < res.length; i++) {
+		table.push([res[i].item_ID, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+	}
+	console.log(table.toString());
+}
